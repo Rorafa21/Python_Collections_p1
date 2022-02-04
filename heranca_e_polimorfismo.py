2712b91607f6682594f9421eec78a3e6c115f0a2
Rodrigo Rafa <rodrigorafa71@gmail.com>
2022-02-04 14:01:11 -0300

    Adicionando for na conta 16 e 17

--- heranca_e_polimorfismo.py
+++ heranca_e_polimorfismo.py
@@ -24,6 +24,11 @@ class ContaPoupanca(Conta):
 
 conta16 = ContaCorrente(16)
 conta16.deposita(1000)
-conta16.passa_o_mes()
-print(conta16)
+conta17 = ContaPoupanca(17)
+conta17.deposita(1000)
 
+contas = [conta16, conta17]
+
+for conta in contas:
+    conta.passa_o_mes()
+    print(conta)
