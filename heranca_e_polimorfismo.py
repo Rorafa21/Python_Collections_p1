ba0e0e0131c4f63c54d182f1b04a213250ac1fe8
Rodrigo Rafa <rodrigorafa71@gmail.com>
2022-02-04 13:38:13 -0300

    Herança e polimorfismo

---
+++ heranca_e_polimorfismo.py
@@ -0,0 +1,29 @@
+class Conta:
+
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return f"[>>Codigo {self._codigo} Saldo {self._saldo}<<<]"
+
+class ContaCorrente(Conta):
+    
+    def passa_o_mes(self):
+        self._saldo -= 2
+
+class ContaPoupanca(Conta):
+
+    def passa_o_mes(self):
+        self._saldo *= 1.01
+        self._saldo -= 3
+
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta16.passa_o_mes()
+print(conta16)
+
