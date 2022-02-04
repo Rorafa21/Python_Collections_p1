1758130b018e78e5f11e783d2df080df2681d6a8
Rodrigo Rafa <rodrigorafa71@gmail.com>
2022-02-04 12:18:21 -0300

    Tuplas

--- objetos_proprios.py
+++ objetos_proprios.py
@@ -1,3 +1,4 @@
+#LISTAS - Mutavel
 class Contacorrente:
 
 
@@ -20,3 +21,12 @@ print(conta_do_gui)
 conta_da_rafa = Contacorrente(47685)
 conta_da_rafa.deposita(1000)
 print(conta_da_rafa)
+
+#TUPLA - Imutavel
+
+guilherme = ("Guilherme", 37, 1981)
+rafaelly = ("Rafaelly", 20, 2001)
+
+#paulo = (21, "Paulo", 2005) - desfaz a ordem.
+#conta_do_gui = (15, 1000) funciona mas não seria possivel alterar valores de forma simples.
+
